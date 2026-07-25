a18e1153d256da7d6808e218aaa1d05adabe4878
Dylan J. Roy-Leo <djroyleo@gmail.com>
2026-07-25 11:15:11 -0400

    Added barebones GUI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions::default();
+
+    eframe::run_ui_native("AeGIS", options, |ui, _frame| {
+        egui::CentralPanel::default()
+            .frame(egui::Frame::new().fill(egui::Color32::WHITE))
+            .show(ui, |ui| {
+                ui.centered_and_justified(|ui| {
+                    if ui.button("EXIT").clicked() {
+                        std::process::exit(0);
+                    }
+                });
+            });
+    })
 }
