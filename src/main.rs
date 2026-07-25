c95d0b0f09dfa9870a608c837688291e0b882fe7
Dylan J. Roy-Leo <djroyleo@gmail.com>
2026-07-25 11:39:11 -0400

    Experimented with adding more widgets to GUI.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,22 @@ use eframe::egui;
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions::default();
+    let mut counter: i32 = 0;
 
-    eframe::run_ui_native("AeGIS", options, |ui, _frame| {
+    eframe::run_ui_native("AeGIS", options, move |ui, _frame| {
         egui::CentralPanel::default()
             .frame(egui::Frame::new().fill(egui::Color32::WHITE))
             .show(ui, |ui| {
-                ui.centered_and_justified(|ui| {
-                    if ui.button("EXIT").clicked() {
-                        std::process::exit(0);
+                if ui.button("EXIT").clicked() {
+                    std::process::exit(0);
+                }
+
+                let half_height = ui.available_height() / 2.0;
+                ui.vertical_centered(|ui| {
+                    ui.add_space(half_height - 30.0);
+                    ui.heading(egui::RichText::new(counter.to_string()).color(egui::Color32::BLACK));
+                    if ui.button("ADD ONE").clicked() {
+                        counter += 1;
                     }
                 });
             });
